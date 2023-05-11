8d121d70031588ba48b6eef39f084719a818c07f
wenlin ye <285889938@qq.com>
2023-05-11 20:29:57 +0200

    animation-lipsync-trial

---
+++ Assets/b.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class b : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        a obj = new a();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/cat.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cat : MonoBehaviour
+{
+    private string name;
+
+    private Color color;
+    // Start is called before the first frame update
+    void Start()
+    {
+        name = "John";
+        color = Color.cyan;
+        Debug.Log($"The name of the cat is {name}");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log($"The color of the cat is {color}");
+        
+    }
+}
